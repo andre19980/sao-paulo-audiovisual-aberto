18796a3dcb1263752a81f9848820131e319aea27
André Solak <akasolak@gmail.com>
2026-08-13 14:47:04 -0300

    Alteração simples de ícones

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ eixo_1_page = st.Page(
 eixo_2_page = st.Page(
   "views/eixo_2/eixo_2_fomento_producao_local_independente.py",
   title="Eixo 2: Fomento à Produção Local e Independente",
-  icon=":material/add_circle:"
+  icon=":material/monitoring:"
 )
 
 pg = st.navigation([eixo_1_page, eixo_2_page])
-st.set_page_config(page_title="Data manager", page_icon=":material/camera:")
+st.set_page_config(page_title="Audiovisual SP Aberto", page_icon=":material/animated_images:")
 pg.run()
